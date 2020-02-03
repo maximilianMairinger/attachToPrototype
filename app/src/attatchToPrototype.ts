f0d0b2f37b9c53d1283e31f3b5ce58dc5bf92d9d
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-02-03 22:11:59 +0100

    code

--- app/src/attatchToPrototype.ts
+++ app/src/attatchToPrototype.ts
@@ -1,5 +1,29 @@
+export function constructAttatchToPrototype(prototype: any) {
+  return function(name: string | string[], func: Function) {
+    const isFunc = typeof func === "function"
+    if (name instanceof Array) {
+      for (let i = 0; i < name.length; i++) {
+        appendToPrototype(name[i], func, isFunc)
+      }
+    }
+    else appendToPrototype(name, func, isFunc)
+  }
 
+  function appendToPrototype(name: string, func: Function, isFunc: boolean) {
+    let ob: any
+    if (isFunc) {
+      ob = {
+        value: func,
+        enumerable: false
+      }
+    }
+    else {
+      ob = func
+      ob.enumerable = false
+    }
 
-export default function() {
-  
+    Object.defineProperty(prototype, name, ob)
+  }
 }
+
+export default constructAttatchToPrototype
