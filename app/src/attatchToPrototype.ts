67d7bc2532cd3a10fdda814ee682e87f183677bc
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-02-03 22:59:48 +0100

    constructApplyToPrototype

--- app/src/attatchToPrototype.ts
+++ app/src/attatchToPrototype.ts
@@ -1,29 +1,74 @@
 export function constructAttatchToPrototype(prototype: any) {
+  let isFunc
+  let f
+  let n
   return function(name: string | string[], func: Function | any) {
-    const isFunc = typeof func === "function"
+    isFunc = typeof func === "function"
+    f = func
+    n = name
     if (name instanceof Array) {
       for (let i = 0; i < name.length; i++) {
-        appendToPrototype(name[i], func, isFunc)
+        appendToPrototype()
       }
     }
-    else appendToPrototype(name, func, isFunc)
+    else appendToPrototype()
   }
 
-  function appendToPrototype(name: string, func: Function | any, isFunc: boolean) {
+  function appendToPrototype() {
     let ob: any
     if (isFunc) {
       ob = {
-        value: func,
+        value: f,
         enumerable: false
       }
     }
     else {
-      ob = func
+      ob = f
       ob.enumerable = false
     }
 
-    Object.defineProperty(prototype, name, ob)
+    Object.defineProperty(prototype, n, ob)
   }
 }
 
 export default constructAttatchToPrototype
+
+export function constructApplyToPrototype(prototype: any) {
+  let isFunc
+  let f
+  let n
+  return function(name: string | string[], func: Function | {get?: () => any, set?: (...to: any) => void}) {
+    isFunc = typeof func === "function"
+    f = func
+    n = name
+    if (name instanceof Array) {
+      for (let i = 0; i < name.length; i++) {
+        appendToPrototype()
+      }
+    }
+    else appendToPrototype()
+  }
+
+  function appendToPrototype() {
+    let ob: any
+    if (isFunc) {
+      ob = {
+        value: f,
+        enumerable: false
+      }
+    }
+    else {
+      ob = {
+        enumerable: false,
+        value: function(...values: any[]) {
+          if (values.length !== 0) f.set.apply(this, values)
+          else return f.get.call(this)
+        }
+      }
+    }
+
+    Object.defineProperty(prototype, n, ob)
+  }
+}
+
+
--- test/src/test.ts
+++ test/src/test.ts
@@ -1,4 +1,15 @@
-import attatchToPrototype from "./../../app/src/attatchToPrototype"
-//const testElem = document.querySelector("#test")
+import {constructApplyToPrototype} from "./../../app/src/attatchToPrototype"
 
-attatchToPrototype()
+let me = constructApplyToPrototype(Array.prototype)
+
+
+me("ok", {get: function() {
+  return this.length
+}})
+
+let w = [2,3]
+debugger
+//@ts-ignore
+console.log(w.ok())
+
+
